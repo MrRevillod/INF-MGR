d7cd9b47ff34a6f2fcbb2b1139dd6dbe3ba87c93
MrRevillod <lrevillod2022@alu.uct.cl>
2025-11-19 18:19:22 -0300

    feat: replace ssh tunnel by vpn

--- apps/server/config/tunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-user="$TUNNEL_SSH_USER"
-ip="$TUNNEL_IP_ADDRESS"
-port="$TUNNEL_PORT"
-
-chmod 600 /app/.ssh/pillan_tunnel
-
-ssh -i "/app/.ssh/pillan_tunnel" -L "$port:$ip:$port" "$user@pillan.inf.uct.cl"
